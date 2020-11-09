9c37979078b813da28a78ec929de67265d883eb4
cccary <798171920@qq.com>
2020-11-09 09:14:46 +0800

    up

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 文件及简介
+ * @Author: Cary
+ * @Date: 2020-11-09 09:13:39
+ * @FilePath: \alien-docse:\work\node-project\learn-code\js\index.js
+ */
+
+// const 和 let 区别
+
+const name = 'Cary'
