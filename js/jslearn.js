14f1acafe6d7b7144efa77e07631aa96c74d7b15
cccary <798171920@qq.com>
2021-04-02 17:26:30 +0800

    up

---
+++ js/jslearn.js
@@ -0,0 +1,18 @@
+/*
+ * @Description: 文件及简介
+ * @Author: Cary
+ * @Date: 2021-03-09 09:10:42
+ * @FilePath: \excel-to-jsone:\work\node-project\learn-code\js\jslearn.js
+ */
+
+// const createBase = num => cur => num + cur
+
+// var addSix = createBase(6);
+// console.log(addSix(10));
+// console.log(addSix(21));
+
+let bb = (function() {
+  console.log('ffff')
+})
+console.log(bb)
+
